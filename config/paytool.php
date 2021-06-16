a5db5a2256b591ea73b05d3e26785b20f8a74fc1
Nick Zhang <castion2293@yahoo.com.tw>
2021-06-16 09:08:25 +0800

    fix: 增加所以付款方式(@nick)

--- config/paytool.php
+++ config/paytool.php
@@ -13,22 +13,40 @@ return [
             'merchant_id' => env('MERCHANT_ID', '2000132'),
             // CheckMacValue加密類型
             'encrypt_type' => '1',
+            // Client 端回 傳付款相關 資訊
+            'client_redirect_url' => env('ClientRedirectURL', ''),
             // 付款類型
             'type' => [
                 // 信用卡
-                'Credit' => [],
+                'Credit' => [
+                    // 分期功能，預設false
+                    'credit_installment_enable' => false,
+                    // 是否使用紅利折抵，預設false
+                    'redeem' => false,
+                    // 是否為聯營卡，預設false
+                    'union_pay' => false
+                ],
 
                 // 網路 ATM
                 'WebATM' => [],
 
                 // 自動櫃員機
-                'ATM' => [],
+                'ATM' => [
+                    //繳費期限 (預設3天，最長60天，最短1天)
+                    'expire_date' => 1,
+                ],
 
                 // 超商代碼
-                'CVS' => [],
+                'CVS' => [
+                    // 超商繳費 截止時間 以分鐘為單位
+                    'store_expire_date' => 1440
+                ],
 
                 // 超商條碼
-                'BARCODE' => [],
+                'BARCODE' => [
+                    // 超商條碼 截止時間 以天為單位
+                    'store_expire_date' => 1
+                ],
             ],
         ],
     ]
--- src/Drivers/EcPayDriver.php
+++ src/Drivers/EcPayDriver.php
@@ -8,9 +8,17 @@ use Pharaoh\Paytool\Exceptions\PaytoolException;
 
 class EcPayDriver extends AbstractDriver
 {
+    /**
+     * EcPay SDK Object
+     *
+     * @var
+     */
+    protected $obj;
+
     public function __construct()
     {
         $this->settings = config('paytool.driver.ec_pay');
+        $this->obj = new ECPay_AllInOne();
     }
 
     /**
@@ -22,26 +30,24 @@ class EcPayDriver extends AbstractDriver
     public function createOrder(array $params)
     {
         try {
-            $obj = new ECPay_AllInOne();
-
             //服務參數
-            $obj->ServiceURL = Arr::get($this->settings, 'service_url');
-            $obj->HashKey = Arr::get($this->settings, 'hash_key');
-            $obj->HashIV = Arr::get($this->settings, 'hash_iv');
-            $obj->MerchantID = Arr::get($this->settings, 'merchant_id');
-            $obj->EncryptType = Arr::get($this->settings, 'encrypt_type');
+            $this->obj->ServiceURL = Arr::get($this->settings, 'service_url');
+            $this->obj->HashKey = Arr::get($this->settings, 'hash_key');
+            $this->obj->HashIV = Arr::get($this->settings, 'hash_iv');
+            $this->obj->MerchantID = Arr::get($this->settings, 'merchant_id');
+            $this->obj->EncryptType = Arr::get($this->settings, 'encrypt_type');
 
             //基本參數(請依系統規劃自行調整)
-            $obj->Send['ReturnURL'] = config('app.url') . '/pay-notice';
-            $obj->Send['MerchantTradeNo'] = Arr::get($params, 'merchant_trade_no');
-            $obj->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');
-            $obj->Send['TotalAmount'] = Arr::get($params, 'total_amount');
-            $obj->Send['TradeDesc'] = Arr::get($params, 'trade_desc');
-            $obj->Send['ChoosePayment'] = Arr::get($params, 'choose_payment');
+            $this->obj->Send['ReturnURL'] = config('app.url') . '/pay-notice';
+            $this->obj->Send['MerchantTradeNo'] = Arr::get($params, 'merchant_trade_no');
+            $this->obj->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');
+            $this->obj->Send['TotalAmount'] = Arr::get($params, 'total_amount');
+            $this->obj->Send['TradeDesc'] = Arr::get($params, 'trade_desc');
+            $this->obj->Send['ChoosePayment'] = $choosePayment =Arr::get($params, 'choose_payment');
 
             //訂單的商品資料
             array_push(
-                $obj->Send['Items'],
+                $this->obj->Send['Items'],
                 [
                     'Name' => Arr::get($params, 'name', ''),
                     'Price' => (int)Arr::get($params, 'price', 0),
@@ -50,17 +56,89 @@ class EcPayDriver extends AbstractDriver
                 ]
             );
 
-            //Credit信用卡分期付款延伸參數(可依系統需求選擇是否代入)
-            //以下參數不可以跟信用卡定期定額參數一起設定
-            $obj->SendExtend['CreditInstallment'] = '' ;    //分期期數，預設0(不分期)，信用卡分期可用參數為:3,6,12,18,24
-            $obj->SendExtend['InstallmentAmount'] = 0 ;    //使用刷卡分期的付款金額，預設0(不分期)
-            $obj->SendExtend['Redeem'] = false ;           //是否使用紅利折抵，預設false
-            $obj->SendExtend['UnionPay'] = false;          //是否為聯營卡，預設false;
+            $functionType = lcfirst(strtolower($choosePayment)) . 'Type';
+            if (method_exists($this, $functionType)) {
+                $this->$functionType($params);
+            }
 
             //產生訂單(auto submit至ECPay)
-            $obj->CheckOut();
+            $this->obj->CheckOut();
         } catch (\Exception $exception) {
             throw new PaytoolException($exception->getMessage());
         }
+    }
+
+    /**
+     * 信用卡付款的專屬參數
+     *
+     * @param array $params
+     */
+    private function creditType(array $params)
+    {
+        //Credit信用卡分期付款延伸參數(可依系統需求選擇是否代入)
+        //以下參數不可以跟信用卡定期定額參數一起設定
+        $creditType = Arr::get($this->settings, 'type.Credit');
+
+        //信用卡分期可用參數為:3,6,12,18,24
+        //使用刷卡分期的付款金額，預設0(不分期)
+        $isCreditInstallmentEnable = Arr::get($creditType, 'credit_installment_enable');
+        $this->obj->SendExtend['CreditInstallment'] = $isCreditInstallmentEnable
+            ? Arr::get($params, 'credit_installment')
+            : '';
+        $this->obj->SendExtend['InstallmentAmount'] = $isCreditInstallmentEnable
+            ? Arr::get($params, 'installment_amount')
+            : 0;
+
+        $this->obj->SendExtend['Redeem'] = Arr::get($creditType, 'redeem');
+        $this->obj->SendExtend['UnionPay'] = Arr::get($creditType, 'union_pay');
+    }
+
+    /**
+     * 自動櫃員機付款的專屬參數
+     *
+     * @param array $params
+     */
+    private function atmType(array $params)
+    {
+        // ATM 延伸參數(可依系統需求選擇是否代入)
+        $atmType = Arr::get($this->settings, 'type.ATM');
+
+        $this->obj->SendExtend['ExpireDate'] = Arr::get($atmType, 'expire_date');
+        $this->obj->SendExtend['PaymentInfoURL'] = config('app.url') . '/pay-information';
+    }
+
+    /**
+     * 超商代碼付款的專屬參數
+     *
+     * @param array $params
+     */
+    private function cvsType(array $params)
+    {
+        // CVS超商代碼延伸參數(可依系統需求選擇是否代入)
+        $cvsType = Arr::get($this->settings, 'type.CVS');
+
+        // 交易描述 會顯示在超商繳費平台的螢幕上。預設空值
+        $this->obj->SendExtend['Desc_1'] = Arr::get($params, 'desc_1', '');
+        $this->obj->SendExtend['Desc_2'] = Arr::get($params, 'desc_2', '');
+        $this->obj->SendExtend['Desc_3'] = Arr::get($params, 'desc_3', '');
+        $this->obj->SendExtend['Desc_4'] = Arr::get($params, 'desc_4', '');
+        $this->obj->SendExtend['PaymentInfoURL'] = config('app.url') . '/pay-information';
+        $this->obj->SendExtend['ClientRedirectURL'] = Arr::get($this->settings, 'client_redirect_url');
+        $this->obj->SendExtend['StoreExpireDate'] = Arr::get($cvsType, 'store_expire_date');
+    }
+
+    private function barcodeType(array $params)
+    {
+        // BARCODE超商條碼延伸參數(可依系統需求選擇是否代入)
+        $barcodeType = Arr::get($this->settings, 'type.BARCODE');
+
+        // 交易描述 會顯示在超商繳費平台的螢幕上。預設空值
+        $this->obj->SendExtend['Desc_1'] = Arr::get($params, 'desc_1', '');
+        $this->obj->SendExtend['Desc_2'] = Arr::get($params, 'desc_2', '');
+        $this->obj->SendExtend['Desc_3'] = Arr::get($params, 'desc_3', '');
+        $this->obj->SendExtend['Desc_4'] = Arr::get($params, 'desc_4', '');
+        $this->obj->SendExtend['PaymentInfoURL'] = config('app.url') . '/pay-information';
+        $this->obj->SendExtend['ClientRedirectURL'] = Arr::get($this->settings, 'client_redirect_url');
+        $this->obj->SendExtend['StoreExpireDate'] = Arr::get($barcodeType, 'store_expire_date');
     }
 }
